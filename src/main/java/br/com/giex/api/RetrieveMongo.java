52450e1781013c306f0f56ae8520d4f40ae0f369
Judilson <judilsonjunior@gmail.com>
2017-06-27 11:56:49 -0300

    Versão inicial

--- src/main/java/br/com/giex/api/RetrieveMongo.java
+++ src/main/java/br/com/giex/api/RetrieveMongo.java
@@ -10,6 +10,7 @@ import com.mongodb.DB;
 import com.mongodb.gridfs.GridFS;
 import com.mongodb.gridfs.GridFSDBFile;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,7 +22,7 @@ import org.bson.types.ObjectId;
  */
 public class RetrieveMongo {
 
-    public File retrieve(String objectID, String banco, String colecao, String usuario, String senha) {
+    public File retrieveFile(String objectID, String banco, String colecao, String usuario, String senha) {
         File temp = null;
         try {
             temp = File.createTempFile("tempfile", ".tmp");
@@ -42,15 +43,11 @@ public class RetrieveMongo {
 
         } catch (IOException e) {
             e.printStackTrace();
-        } 
-//        finally {
-//            temp.delete();
-//        }
-
+        }
         return temp;
     }
 
-    public File retrieve(String key, Object value, String banco, String colecao, String usuario, String senha) {
+    public File retrieveFile(String key, Object value, String banco, String colecao, String usuario, String senha) {
         File temp = null;
         try {
             temp = File.createTempFile("tempfile", ".tmp");
@@ -71,12 +68,50 @@ public class RetrieveMongo {
 
         } catch (IOException e) {
             e.printStackTrace();
-        } 
-//        finally {
-//            temp.delete();
-//        }
+        }
 
         return temp;
     }
 
+    public byte[] retrieveByte(String objectID, String banco, String colecao, String usuario, String senha) {
+        File temp = null;
+        byte[] bytesArray = null;
+        FileInputStream fileInputStream = null;
+        try {
+            temp = File.createTempFile("tempfile", ".tmp");
+        } catch (IOException ex) {
+            Logger.getLogger(RetrieveMongo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+
+            BasicDBObject basicDBObject = new BasicDBObject();
+            basicDBObject.put("_id", new ObjectId(objectID));
+
+            DB db = Connection.getInstance().getClient(Connection.ENDERECOLEITURA2, Connection.PORTALEITURA2, usuario, senha).getDB(banco);
+
+            GridFS gridFS = new GridFS(db, colecao);
+            GridFSDBFile gridFSDBFile = gridFS.findOne(basicDBObject);
+
+            gridFSDBFile.writeTo(temp);
+
+            bytesArray = new byte[(int) temp.length()];
+
+            //read file into bytes[]
+            fileInputStream = new FileInputStream(temp);
+            fileInputStream.read(bytesArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return bytesArray;
+    }
 }
