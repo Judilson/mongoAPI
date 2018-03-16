51d15d1c0331e2f10468248b251bfdfa23137c00
Judilson José da Costa Júnior <judilsonjunior@gmail.com>
2018-03-16 15:53:51 -0300

    autenticação mongo

--- src/main/java/br/com/giex/api/Connection.java
+++ src/main/java/br/com/giex/api/Connection.java
@@ -6,6 +6,10 @@
 package br.com.giex.api;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,13 +17,13 @@ import com.mongodb.MongoClient;
  */
 public class Connection {
 
-    public static final String ENDERECOESCRITA = "173.22.21.22";
-    public static final String ENDERECOLEITURA1 = "173.22.21.24";
-    public static final String ENDERECOLEITURA2 = "173.22.21.28";
+    public static final String ENDERECOESCRITA = "173.22.21.29";//"173.22.21.22";
+    public static final String ENDERECOLEITURA1 = "173.22.21.38";//"173.22.21.24";
+    public static final String ENDERECOLEITURA2 = "173.22.21.36";//"173.22.21.28";
 
     public static final int PORTAESCRITA = 27017;
-    public static final int PORTALEITURA1 = 27018;
-    public static final int PORTALEITURA2 = 27019;
+    public static final int PORTALEITURA1 = 27017;//27018;
+    public static final int PORTALEITURA2 = 27017;//27019;
 
     private static Connection uniqInstance;
 
@@ -37,7 +41,18 @@ public class Connection {
 
     public MongoClient getClient(String endereco, int porta, String usuario, String senha) {
         if (mongoClient == null) {
-            mongoClient = new MongoClient(endereco, porta);
+
+            List<ServerAddress> seeds = new ArrayList<ServerAddress>();
+            seeds.add(new ServerAddress(endereco,porta));
+            List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+            credentials.add(
+                    MongoCredential.createCredential(
+                            usuario,
+                            "giex",
+                            senha.toCharArray()
+                    )
+            );
+            mongoClient = new MongoClient(seeds,credentials);
         }
         return mongoClient;
     }
